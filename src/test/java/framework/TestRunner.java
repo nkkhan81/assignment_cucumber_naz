a897a79f342b5a0946f4e8a7a2baa00414ffc2ba
Nazmul Khan <nkkhan81@gmail.com>
2018-03-24 10:49:03 -0400

    testRunner Class updated

--- src/test/java/framework/TestRunner.java
+++ src/test/java/framework/TestRunner.java
@@ -17,7 +17,7 @@ import org.junit.runner.RunWith;
         features = {"src/test/resources/features"},
 
         // Specify tags to be executed
-        tags = {"@hotel-TA7-3"},
+        tags = {"@calendar"},
 
         //Specify step definition package name (Note: make sure to have this package on current directory)
         glue = {"stepdefinition"}
